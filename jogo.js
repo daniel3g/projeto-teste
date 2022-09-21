fa2d88f03bd4a8e019b25626a401acc08bda0f39
daniel3g <silvadaniel2006@yahoo.com.br>
2022-09-21 15:46:18 -0300

    first commit

---
+++ jogo.js
@@ -0,0 +1,69 @@
+function getRandomInt(min, max) {
+    min = Math.ceil(1000);
+    max = Math.floor(9999);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+randomNaumber = getRandomInt();
+
+var acertos = [];
+var number = String(randomNaumber).split("").map((randomNaumber)=>{
+    return Number(randomNaumber);
+  });
+
+number1 = number[0];
+number2 = number[1];
+number3 = number[2];
+number4 = number[3];
+
+var palpite = new Array(1,3,4,5);
+
+palpite1 = palpite[0];
+palpite2 = palpite[1];
+palpite3 = palpite[2];
+palpite4 = palpite[3];
+
+if(number1 === palpite1){
+    acertos.push(1);
+    number.splice(0, 1);
+    palpite.splice(0, 1);
+}
+
+if(number2 === palpite2){
+    acertos.push(1);
+    number.splice(1, 1);
+    palpite.splice(1, 1);
+}
+
+if(number3 === palpite3){
+    acertos.push(1);
+    number.splice(2, 1);
+    palpite.splice(2, 1);
+}
+
+if(number4 === palpite4){
+    acertos.push(1);
+    number.pop();
+    palpite.pop();
+}
+
+for (var i=0; i < number.length; i++){
+    for (var j=0; j < number.length; j++){
+        if(number[i] === palpite[j]){
+            acertos.push(0);
+        }
+    }
+}
+
+console.log(number);
+
+/*
+
+if (acertos[0] === 1 && acertos[1] === 1 && acertos[2] === 1 && acertos[3] === 1){
+    console.log("Você acertou");
+}else{
+    console.log(acertos);
+}
+
+*/
+
