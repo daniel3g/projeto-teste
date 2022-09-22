6941f8bdd5cc1d2169c5409c4ba904bb8d9f9666
daniel3g <silvadaniel2006@yahoo.com.br>
2022-09-21 22:14:17 -0300

    melhorando a lógica

--- jogo.js
+++ jogo.js
@@ -16,54 +16,76 @@ number2 = number[1];
 number3 = number[2];
 number4 = number[3];
 
-var palpite = new Array(1,3,4,5);
+const btn = document.querySelector("#button-send");
 
-palpite1 = palpite[0];
-palpite2 = palpite[1];
-palpite3 = palpite[2];
-palpite4 = palpite[3];
+btn.addEventListener("click", function(e) {
 
-if(number1 === palpite1){
-    acertos.push(1);
-    number.splice(0, 1);
-    palpite.splice(0, 1);
-}
+    e.preventDefault();
 
-if(number2 === palpite2){
-    acertos.push(1);
-    number.splice(1, 1);
-    palpite.splice(1, 1);
-}
+    var input = document.querySelector("#input-palpite");
+    var palpiteInput = input.value;
 
-if(number3 === palpite3){
-    acertos.push(1);
-    number.splice(2, 1);
-    palpite.splice(2, 1);
-}
+    var palpite = String(palpiteInput).split("").map((palpiteInput)=>{
+        return Number(palpiteInput);
+      });
+    
+    //console.log(palpite);
+    console.log(number);
 
-if(number4 === palpite4){
-    acertos.push(1);
-    number.pop();
-    palpite.pop();
-}
+    palpite1 = palpite[0];
+    palpite2 = palpite[1];
+    palpite3 = palpite[2];
+    palpite4 = palpite[3];
 
-for (var i=0; i < number.length; i++){
-    for (var j=0; j < number.length; j++){
-        if(number[i] === palpite[j]){
-            acertos.push(0);
+    if(number1 === palpite1){
+        //acertos.push(1);
+        //number.splice(0, 1);
+        //palpite.splice(0, 1);
+    }
+
+    if(number2 === palpite2){
+       // acertos.push(1);
+        //number.splice(1, 1);
+        //palpite.splice(1, 1);
+    }
+
+    if(number3 === palpite3){
+        //acertos.push(1);
+        //number.splice(2, 1);
+        //palpite.splice(2, 1);
+    }
+
+    if(number4 === palpite4){
+        //acertos.push(1);
+        //number.pop();
+        //palpite.pop();
+    }
+
+    for (var i=0; i < number.length; i++){
+        for (var j=0; j < number.length; j++){
+            if((number[i] === palpite[j]) && (i === j)){
+                acertos.push(1);
+            }else if((number[i] === palpite[j]) && (i != j)){
+                acertos.push(0);
+            }
         }
     }
-}
 
-console.log(number);
+    if (acertos[0] === 1 && acertos[1] === 1 && acertos[2] === 1 && acertos[3] === 1){
+        console.log("Você acertou");
+    }else{
+        console.log(acertos);
+        acertos = [];
+    }
 
-/*
+    
+});
 
-if (acertos[0] === 1 && acertos[1] === 1 && acertos[2] === 1 && acertos[3] === 1){
-    console.log("Você acertou");
-}else{
-    console.log(acertos);
-}
 
-*/
+
+
+
+
+
+
 
